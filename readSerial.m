39e0db1aa8bda461472c8aad34f4bb2732554a8b
U-Brooks6\Brooks6 <somj94@gmail.com>
2019-04-19 12:11:59 -0400

    initial commit

---
+++ readSerial.m
@@ -0,0 +1,18 @@
+%% TESTS
+clear all
+clc
+
+delete(instrfind);
+
+ arduino=serial('COM3','BaudRate',9600);
+ fopen(arduino); 
+ 
+ pause(1)
+
+while 1
+    new_str = split(fscanf(arduino,'%s'),',');
+    disp(new_str);
+end
+
+ 
+ fclose(arduino); % end communication with arduino
